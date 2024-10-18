599f53d0d205da12d8a6a4307d4363b64a0fc4b2
Diego Alexander Contreras Duarte <dcontrerasd>
2024-10-18 14:24:22 -0600

    Creacion de boton

--- app/src/main/java/com/example/appdemascotas/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/example/appdemascotas/ui/screen/HomeScreen.kt
@@ -2,18 +2,22 @@ package com.example.appdemascotas.ui.screen
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.material3.Text as Text
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
@@ -34,6 +38,7 @@ fun HomeSceen() {
 }
 
 @Composable
+fun Header(name: String = "Mascotas") {
     Row(
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically, // Usar Alignment en lugar de Layout
@@ -48,7 +53,10 @@ fun HomeSceen() {
                 color = Color.White
             )
             Text(
+                text = "Estamos Creando este programa para el Pryecto Final de Programacion , $name",
                 style = MaterialTheme.typography.bodyLarge, // Cambié de h2 a bodyLarge
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp, // Puedes cambiar el tamaño de la fuente si es necesario
                 color = Color.White
             )
         }
